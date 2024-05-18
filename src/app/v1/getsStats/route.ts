f23c62c585ec75122c5e2c28c5f3097af6471bb0
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-05-18 16:22:07 +0100

    feat(API): Create a getstats module

---
+++ src/app/v1/getsStats/route.ts
@@ -0,0 +1,43 @@
+import { getStats } from "@/modules/Utils/Stats";
+import { type NextRequest } from "next/server";
+
+/*
+Copyright © 2024 Kars (github.com/kars1996)
+
+Not to be shared, replicated or used without prior consent.
+Contact Kars for any enquieries
+*/
+
+interface ResponseProp {
+    response: any;
+    status?: number;
+}
+
+const stats = getStats()
+const key = process.env.TOKEN || "admin"
+
+const Data: ResponseProp = {
+    response: "Unatuhorised Request",
+    status: 200,
+};
+
+export function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("key");
+    
+    if (query?.toLowerCase() === key) {
+        return new Response(
+            JSON.stringify({ response: stats, status: 200 }),
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+    }
+    return new Response(JSON.stringify(Data), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
