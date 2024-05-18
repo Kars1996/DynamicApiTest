c1a1aa225d3b318fe9b700b28f0226f53e14e505
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-05-18 16:19:05 +0100

    chore(DB): Added Postgress Feature

--- src/modules/Utils/Stats.ts
+++ src/modules/Utils/Stats.ts
@@ -19,5 +19,15 @@ export async function getStats() {
 }
 
 async function updateStatsInDatabase(statType: string, value: number) {
+    const { rowCount } =
+        await sql`SELECT EXISTS(SELECT 1 FROM pg_tables WHERE tablename='stats')`;
+    if (!rowCount) {
+        await sql`
+            CREATE TABLE IF NOT EXISTS stats (
+                id SERIAL PRIMARY KEY,
+                successfulRequests INTEGER DEFAULT 0,
+                failedRequests INTEGER DEFAULT 0
+            )`;
+    }
     await sql`UPDATE stats SET ${statType} = $1 WHERE id = 1`, [value];
 }
